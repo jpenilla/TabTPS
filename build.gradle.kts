71fd5a5a2f3c300a0f655a20c639ea4cc9df44b9
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-08-10 18:05:13 -0700

    1.3.10-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "xyz.jpenilla"
-version = "1.3.9"
+version = "1.3.10-SNAPSHOT"
   .run { if (endsWith("-SNAPSHOT")) "$this+${lastCommitHash()}" else this }
 description = "Monitor your server's performance in real time"
 
