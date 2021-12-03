ab7a5a44d4ab6449bd868ef12a5ecd018f06fcc4
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-12-02 22:12:24 -0800

    1.3.12-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "xyz.jpenilla"
-version = "1.3.11".decorateVersion()
+version = "1.3.12-SNAPSHOT".decorateVersion()
 description = "Monitor your server's performance in real time"
 
 subprojects {
