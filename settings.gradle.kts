3237406542f99f02fc09680d124538518689cc10
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-05-10 17:12:57 -0700

    Update adventure-platform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,6 @@ dependencyResolutionManagement {
         snapshotsOnly()
         includeGroup("xyz.jpenilla")
         includeGroup("org.incendo")
-        includeGroup("net.kyori") // TODO adventure mod 6.4.0 & platform 4.3.5
       }
     }
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
